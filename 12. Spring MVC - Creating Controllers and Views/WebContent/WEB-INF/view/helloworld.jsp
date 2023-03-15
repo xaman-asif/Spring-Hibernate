cbbf133c9bdd9f5e23db4e1bb31106e73c15fb89
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-03-15 21:43:11 +0600

    Spring MVC - Creating Controllers and Views

---
+++ 12. Spring MVC - Creating Controllers and Views/WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		Hello World of Spring
+	</head>
+	<body>
+	<br/>
+		<!-- <p>Student name: ${param.studentName}</p> -->
+		
+		The message: ${message}
+	</body>
+</html>
---
+++ 12. Spring MVC - Creating Controllers and Views/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		
+		// convert data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+}
