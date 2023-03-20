2eb434879aa1a44ead2292996c6d054fbd45e3e8
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-03-20 23:58:13 +0600

    Spring MVC Form Validation - Creating Custom Validation Rules

---
+++ 17. Spring MVC Form Validation - Creating Custom Validation Rules/src/com/luv2code/springdemo/mvc/customer/Customer.java
@@ -0,0 +1,71 @@
+package com.luv2code.springdemo.mvc.customer;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
+public class Customer {
+	
+	@NotNull(message="Firstname is required")
+	@Size(min=1, message="Firstname is required")
+	private String firstName;
+	
+	@NotNull(message="Lastname is required")
+	@Size(min=1, message="Lastname is required")
+	private String lastName;
+	
+	@NotNull(message="is required")
+	@Min(value=0, message="must be greater or equal to zero")
+	@Max(value=10, message="must be lesser or equal to ten")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
+	@CourseCode(value="TOPS", message="must start with TOPS")
+	private String courseCode;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+}
