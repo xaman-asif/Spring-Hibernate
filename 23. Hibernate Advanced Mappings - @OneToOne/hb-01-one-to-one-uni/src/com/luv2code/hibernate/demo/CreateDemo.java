4a8d07d4ffa764f3a69c818ca4a1c1544b9ad7bf
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-03-27 11:19:36 +0600

    Hibernate Advanced Mappings - @OneToOne

---
+++ 23. Hibernate Advanced Mappings - @OneToOne/hb-01-one-to-one-uni/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,67 @@
+package com.luv2code.hibernate.demo;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// use the session object to save Java object
+			// create a the object
+			Instructor tempInstructor1 = 
+					new Instructor("Chad", "Darby", "darby@luv2code.com");
+			InstructorDetail tempInstructorDetail1 =
+					new InstructorDetail(
+							"http://www.luv2code.com/youtube",
+							"I Love to Code!!!"
+							);
+			
+			Instructor tempInstructor2 = 
+					new Instructor("Simp", "Son", "simp@luv2code.com");
+			InstructorDetail tempInstructorDetail2 =
+					new InstructorDetail(
+							"http://www.luv2code.com/simpson",
+							"I Love to Simp!!!"
+							);
+			
+			// associate the objects
+			tempInstructor1.setInstructorDetail(tempInstructorDetail1);
+			tempInstructor2.setInstructorDetail(tempInstructorDetail2);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor object
+			System.out.println("Saving the instructor: " + tempInstructor1);
+			session.save(tempInstructor1);
+			
+			System.out.println("Saving the instructor: " + tempInstructor2);
+			session.save(tempInstructor2);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done");
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			factory.close();
+		}
+	}
+}
