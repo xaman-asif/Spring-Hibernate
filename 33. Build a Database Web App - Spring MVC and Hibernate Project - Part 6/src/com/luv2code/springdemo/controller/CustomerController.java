6ecf2a06ea74d0d3e17e7bd95d0a419d6bcec7bb
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-04-12 00:20:36 +0600

    Build a Database Web App - Spring MVC and Hibernate Project - Part 6

---
+++ 33. Build a Database Web App - Spring MVC and Hibernate Project - Part 6/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,86 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// need to inject our customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
+//	@PostMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		// get customers from the customerService
+		
+		List<Customer> theCustomers = customerService.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);
+		
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId, 
+									Model theModel) {
+		Customer theCustomer = customerService.getCustomer(theId);
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId, 
+									Model theModel) {
+		customerService.deleteCustomer(theId);
+		
+		//theModel.addAttribute("customer", theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/search")
+	public String deleteCustomer(@RequestParam("theSearchName") String theSearchName, 
+									Model theModel) {
+		List<Customer> theCustomers = customerService.searchCustomers(theSearchName);
+		
+		theModel.addAttribute("customers", theCustomers);
+		
+		return "list-customers";
+	}
+}
