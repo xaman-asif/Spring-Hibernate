2f5008be9054b7fa819818d4a3dc6aaf8c3aaaa4
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-04-11 15:44:47 +0600

    Build a Database Web App - Spring MVC and Hibernate Project - Part 5

---
+++ 32. Build a Database Web App - Spring MVC and Hibernate Project - Part 5/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,63 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	public List<Customer> getCustomers() {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// create a query
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer order by lastName", Customer.class);
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+		// return the results
+		return customers;
+	}
+
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		// TODO Auto-generated method stub
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		// save the customer 
+		currentSession.saveOrUpdate(theCustomer);
+	}
+
+
+	@Override
+	public Customer getCustomer(int theId) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Customer> theQuery = 
+		currentSession.createQuery("from Customer where id = " + theId, Customer.class);
+		// execute query and get result list
+		Customer theCustomer0 = theQuery.getSingleResult();
+		
+		//Customer theCustomer1 = currentSession.get(Customer.class, theId);
+		// return the results
+		return theCustomer0;
+	}
+
+}
