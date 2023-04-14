7f2eb2791ec70784d1b5f94bee756f1bda704a71
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-04-14 23:31:59 +0600

    Spring REST - CRUD Database Real-Time Project - Update Customer

---
+++ 67. Spring REST - CRUD Database Real-Time Project - Update Customer/src/main/java/com/luv2code/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
